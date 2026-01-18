9e62019d3397e260b6500ea6b6d01797f9fd35cc
badhon_pain <badhonpain48@gmail.com>
2026-01-18 08:43:00 +0600

    Time: 0 ms (100%), Space: 11.8 MB (50.87%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            if (ch == '(')
+                st.push(i);
+
+            else if (ch == ')') {
+                st.pop();
+                if (st.empty())
+                    st.push(i);
+                else
+                    maxLen = max(maxLen, i - st.top());
+            }
+        }
+        return maxLen;
+    }
+};
